c1070c8844fd6b96d00ef0cbad3fce39bb8a2f08
Iman Jawad <imanj12@gmail.com>
2018-07-16 16:55:04 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -3,7 +3,7 @@ function forLoop(array) {
     if ( i === 1) {
       array = [...array, "I am 1 strange loop."]
     } else {
-      array = [...array, "I am ${i} strange loops."]
+      array = [...array, `I am ${i} strange loops.`]
     }
   }
   return array
